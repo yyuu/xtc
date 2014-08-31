3a8a371a6e65c838ca3f710bb9dafdac3953bd89
Yamashita Yuu <peek824545201@gmail.com>
2014-08-31 20:29:45 +0900

    Add TestFor1

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -57,9 +57,13 @@ gets( );
       []ast.IStmtNode {
         ast.NewExprStmtNode(loc(1,1),
           ast.NewFuncallNode(loc(1,1),
-                          ast.NewVariableNode(loc(1,1), "gets"),
-                          []ast.IExprNode {
-                          })),
+            ast.NewVariableNode(loc(1,1),
+              "gets",
+            ),
+            []ast.IExprNode {
+            },
+          ),
+        ),
       },
     },
   )
@@ -74,10 +78,16 @@ func TestParseFuncallWithSingleArgument(t *testing.T) {
       []ast.IStmtNode {
         ast.NewExprStmtNode(loc(1,5),
           ast.NewFuncallNode(loc(1,5),
-                          ast.NewVariableNode(loc(1,5), "println"),
-                          []ast.IExprNode {
-                            ast.NewStringLiteralNode(loc(1,13), "\"hello, world\""),
-                          })),
+            ast.NewVariableNode(loc(1,5),
+              "println",
+            ),
+            []ast.IExprNode {
+              ast.NewStringLiteralNode(loc(1,13),
+                "\"hello, world\"",
+              ),
+            },
+          ),
+        ),
       },
     },
   )
@@ -90,18 +100,74 @@ func TestParseFuncallWithMultipleArguments(t *testing.T) {
       "hello, %s",
       "world"
     );
-
   `
   assertEqualsAST(t, testParse(t, s),
     ast.AST {
       []ast.IStmtNode {
         ast.NewExprStmtNode(loc(2,5),
           ast.NewFuncallNode(loc(2,5),
-                          ast.NewVariableNode(loc(2,5), "println"),
-                          []ast.IExprNode {
-                            ast.NewStringLiteralNode(loc(3,7), "\"hello, %s\""),
-                            ast.NewStringLiteralNode(loc(4,7), "\"world\""),
-                          })),
+            ast.NewVariableNode(loc(2,5),
+              "println",
+            ),
+            []ast.IExprNode {
+              ast.NewStringLiteralNode(loc(3,7),
+                "\"hello, %s\"",
+              ),
+              ast.NewStringLiteralNode(loc(4,7),
+                "\"world\"",
+              ),
+            },
+          ),
+        ),
+      },
+    },
+  )
+}
+
+func TestFor1(t *testing.T) {
+  s := `
+    for (i=0; i<100; i++) println(i);
+`
+  assertEqualsAST(t, testParse(t, s),
+    ast.AST {
+      []ast.IStmtNode {
+        ast.NewForNode(loc(1,5),
+          ast.NewAssignNode(loc(1,10),
+            ast.NewVariableNode(loc(1,10),
+              "i",
+            ),
+            ast.NewIntegerLiteralNode(loc(1,12),
+              "0",
+            ),
+          ),
+          ast.NewBinaryOpNode(loc(1,15),
+            "<",
+            ast.NewVariableNode(loc(1,15),
+              "i",
+            ),
+            ast.NewIntegerLiteralNode(loc(1,17),
+              "100",
+            ),
+          ),
+          ast.NewSuffixOpNode(loc(1,22),
+            "++",
+            ast.NewVariableNode(loc(1,22),
+              "i",
+            ),
+          ),
+          ast.NewExprStmtNode(loc(1,27),
+            ast.NewFuncallNode(loc(1,27),
+              ast.NewVariableNode(loc(1,27),
+                "println",
+              ),
+              []ast.IExprNode {
+                ast.NewVariableNode(loc(1,35),
+                  "i",
+                ),
+              },
+            ),
+          ),
+        ),
       },
     },
   )
