146607ff0c09d15301db29ca5bf299c670ae7fdb
Yamashita Yuu <peek824545201@gmail.com>
2014-09-30 20:03:26 +0900

    Add `compiler.Compiler` and move functions from main.go

---
+++ compiler/compiler.go
@@ -0,0 +1,134 @@
+package compiler
+
+import (
+  "encoding/json"
+  "fmt"
+  bs_ast "bitbucket.org/yyuu/bs/ast"
+  bs_core "bitbucket.org/yyuu/bs/core"
+  bs_ir "bitbucket.org/yyuu/bs/ir"
+  bs_parser "bitbucket.org/yyuu/bs/parser"
+  bs_sysdep "bitbucket.org/yyuu/bs/sysdep"
+  bs_typesys "bitbucket.org/yyuu/bs/typesys"
+)
+
+type Compiler struct {
+  errorHandler *bs_core.ErrorHandler
+  options *bs_core.Options
+}
+
+func NewCompiler(name string, args []string) *Compiler {
+  options := bs_core.ParseOptions(name, args)
+  var logLevel = bs_core.LOG_INFO
+  if options.IsVerboseMode() {
+    logLevel = bs_core.LOG_DEBUG
+  }
+  errorHandler := bs_core.NewErrorHandler(logLevel)
+  return &Compiler {
+    errorHandler: errorHandler,
+    options: options,
+  }
+}
+
+func (self *Compiler) SourceFiles() []string {
+  return self.options.SourceFiles()
+}
+
+func (self *Compiler) Compile() {
+  files := self.SourceFiles()
+  for i := range files {
+    ast, err := bs_parser.ParseFile(files[i], self.errorHandler, self.options)
+    if err != nil {
+      panic(err)
+    }
+    self.evaluateAST(ast)
+  }
+}
+
+func (self *Compiler) CompileString(source string) {
+  ast, err := bs_parser.ParseExpr(source, self.errorHandler, self.options)
+  if err != nil {
+    panic(err)
+  }
+  self.evaluateAST(ast)
+}
+
+func (self *Compiler) evaluateAST(ast *bs_ast.AST) {
+  self.dumpAST(ast)
+  types := bs_typesys.NewTypeTableFor(self.options.TargetPlatform())
+  sem := self.semanticAnalyze(ast, types)
+  self.dumpSemant(sem)
+  ir := NewIRGenerator(self.errorHandler, self.options, types).Generate(sem)
+  self.dumpIR(ir)
+  asm := self.generateAssembly(ir)
+  self.dumpAsm(asm)
+  self.printAsm(asm)
+}
+
+func (self *Compiler) semanticAnalyze(ast *bs_ast.AST, types *bs_typesys.TypeTable) *bs_ast.AST {
+  NewLocalResolver(self.errorHandler, self.options).Resolve(ast)
+  NewTypeResolver(self.errorHandler, self.options, types).Resolve(ast)
+  types.SemanticCheck(self.errorHandler)
+  NewDereferenceChecker(self.errorHandler, self.options, types).Check(ast)
+  NewTypeChecker(self.errorHandler, self.options, types).Check(ast)
+  return ast
+}
+
+func (self *Compiler) generateAssembly(ir *bs_ir.IR) bs_sysdep.AssemblyCode {
+  code_generator := bs_sysdep.NewCodeGeneratorFor(self.errorHandler, self.options, self.options.TargetPlatform())
+  return code_generator.Generate(ir)
+}
+
+func (self *Compiler) dumpAST(ast *bs_ast.AST) {
+  if ! self.options.DumpAST() {
+    return
+  }
+  cs, err := json.MarshalIndent(ast, "", "  ")
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("// AST")
+  fmt.Println(string(cs))
+}
+
+func (self *Compiler) dumpSemant(ast *bs_ast.AST) {
+  if ! self.options.DumpSemantic() {
+    return
+  }
+  cs, err := json.MarshalIndent(ast, "", "  ")
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("// Semantics")
+  fmt.Println(string(cs))
+}
+
+func (self *Compiler) dumpIR(ir *bs_ir.IR) {
+  if ! self.options.DumpIR() {
+    return
+  }
+  cs, err := json.MarshalIndent(ir, "", "  ")
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("// IR")
+  fmt.Println(string(cs))
+}
+
+func (self *Compiler) dumpAsm(asm bs_sysdep.AssemblyCode) {
+  if ! self.options.DumpAsm() {
+    return
+  }
+  cs, err := json.MarshalIndent(asm, "", "  ")
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("// Asm")
+  fmt.Println(string(cs))
+}
+
+func (self *Compiler) printAsm(asm bs_sysdep.AssemblyCode) {
+  if ! self.options.PrintAsm() {
+    return
+  }
+  fmt.Println(asm.ToSource())
+}
--- main.go
+++ main.go
@@ -2,142 +2,40 @@ package main
 
 import (
   "bufio"
-  "encoding/json"
   "fmt"
   "os"
   "strings"
-  bs_ast "bitbucket.org/yyuu/bs/ast"
   bs_compiler "bitbucket.org/yyuu/bs/compiler"
-  bs_core "bitbucket.org/yyuu/bs/core"
-  bs_ir "bitbucket.org/yyuu/bs/ir"
-  bs_parser "bitbucket.org/yyuu/bs/parser"
-  bs_sysdep "bitbucket.org/yyuu/bs/sysdep"
-  bs_typesys "bitbucket.org/yyuu/bs/typesys"
 )
 
-var errorHandler = bs_core.NewErrorHandler(bs_core.LOG_DEBUG)
-
 func main() {
-  options := bs_core.ParseOptions(os.Args[0], os.Args[1:])
-  files := options.SourceFiles()
+  compiler := bs_compiler.NewCompiler(os.Args[0], os.Args[1:])
+  files := compiler.SourceFiles()
   if 0 < len(files) {
-    for i := range files {
-      ast, err := bs_parser.ParseFile(files[i], errorHandler, options)
-      ep(ast, err, options)
-    }
+    compiler.Compile()
   } else {
-    repl(options)
+    repl(compiler)
   }
 }
 
-func repl(options *bs_core.Options) {
+func repl(compiler *bs_compiler.Compiler) {
   defer func() {
     if s := recover(); s != nil {
       fmt.Fprintf(os.Stderr, "recovered: %s\n", s)
-      repl(options)
+      repl(compiler)
     }
   }()
-  in  := bufio.NewReader(os.Stdin)
-  out := bufio.NewWriter(os.Stdout)
+  stdin  := bufio.NewReader(os.Stdin)
+  stdout := bufio.NewWriter(os.Stdout)
   for {
-    s := r(in, out, options)
-    if s != "" {
-      ast, err := bs_parser.ParseExpr(s, errorHandler, options)
-      ep(ast, err, options)
+    stdout.WriteString("> ")
+    stdout.Flush()
+    source, err := stdin.ReadString('\n')
+    if err != nil {
+      os.Exit(1)
+    }
+    if strings.TrimSpace(source) != "" {
+      compiler.CompileString(source)
     }
   }
-}
-
-func r(in *bufio.Reader, out *bufio.Writer, options *bs_core.Options) string {
-  out.WriteString("> ")
-  out.Flush()
-  s, err := in.ReadString('\n')
-  if err != nil {
-    os.Exit(1)
-  }
-  return strings.TrimSpace(s)
-}
-
-func ep(ast *bs_ast.AST, err error, options *bs_core.Options) {
-  if err != nil {
-    panic(err)
-  }
-  dumpAST(ast, options)
-  types := bs_typesys.NewTypeTableFor(options.TargetPlatform())
-  sem := semanticAnalyze(ast, types, options)
-  dumpSemant(sem, options)
-  ir := bs_compiler.NewIRGenerator(errorHandler, options, types).Generate(sem)
-  dumpIR(ir, options)
-  asm := generateAssembly(ir, options)
-  dumpAsm(asm, options)
-  printAsm(asm, options)
-}
-
-func semanticAnalyze(ast *bs_ast.AST, types *bs_typesys.TypeTable, options *bs_core.Options) *bs_ast.AST {
-  bs_compiler.NewLocalResolver(errorHandler, options).Resolve(ast)
-  bs_compiler.NewTypeResolver(errorHandler, options, types).Resolve(ast)
-  types.SemanticCheck(errorHandler)
-  bs_compiler.NewDereferenceChecker(errorHandler, options, types).Check(ast)
-  bs_compiler.NewTypeChecker(errorHandler, options, types).Check(ast)
-  return ast
-}
-
-func generateAssembly(ir *bs_ir.IR, options *bs_core.Options) bs_sysdep.AssemblyCode {
-  code_generator := bs_sysdep.NewCodeGeneratorFor(errorHandler, options, options.TargetPlatform())
-  return code_generator.Generate(ir)
-}
-
-func dumpAST(ast *bs_ast.AST, options *bs_core.Options) {
-  if ! options.DumpAST() {
-    return
-  }
-  cs, err := json.MarshalIndent(ast, "", "  ")
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("// AST")
-  fmt.Println(string(cs))
-}
-
-func dumpSemant(ast *bs_ast.AST, options *bs_core.Options) {
-  if ! options.DumpSemantic() {
-    return
-  }
-  cs, err := json.MarshalIndent(ast, "", "  ")
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("// Semantics")
-  fmt.Println(string(cs))
-}
-
-func dumpIR(ir *bs_ir.IR, options *bs_core.Options) {
-  if ! options.DumpIR() {
-    return
-  }
-  cs, err := json.MarshalIndent(ir, "", "  ")
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("// IR")
-  fmt.Println(string(cs))
-}
-
-func dumpAsm(asm bs_sysdep.AssemblyCode, options *bs_core.Options) {
-  if ! options.DumpAsm() {
-    return
-  }
-  cs, err := json.MarshalIndent(asm, "", "  ")
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("// Asm")
-  fmt.Println(string(cs))
-}
-
-func printAsm(asm bs_sysdep.AssemblyCode, options *bs_core.Options) {
-  if ! options.PrintAsm() {
-    return
-  }
-  fmt.Println(asm.ToSource())
 }
